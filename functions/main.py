05a12b0a4a6b38fdce08d07eb8bfe80720ff3747
emanfolo <emmanuelfolorunsho@icloud.com>
2023-12-06 18:03:23 +0000

    Begin work on backend algo - Create rating system - Create weighting system for random selection

--- functions/main.py
+++ functions/main.py
@@ -2,6 +2,7 @@ from firebase_functions import https_fn
 from firebase_admin import initialize_app, firestore
 from flask import jsonify
 import google.cloud.firestore
+import random
 
 
 app = initialize_app()
@@ -9,10 +10,9 @@ app = initialize_app()
 
 @https_fn.on_request()
 def get_recipes(req: https_fn.Request) -> https_fn.Response:
-
     firestore_client: google.cloud.firestore.Client = firestore.client()
 
-    collection_ref = firestore_client.collection('recipes')
+    collection_ref = firestore_client.collection("recipes")
 
     # Query the collection and limit the result to the first 20 documents
     query = collection_ref.limit(20)
@@ -25,37 +25,122 @@ def get_recipes(req: https_fn.Request) -> https_fn.Response:
     return parsed_response
 
 
+@https_fn.on_request()
+def generate_meal_plan(req: https_fn.Request) -> https_fn.Response:
+    firestore_client: google.cloud.firestore.Client = firestore.client()
+
+    collection_ref = firestore_client.collection("recipes")
+
+    # User Details
+    user_details = {
+        "weight": 80,
+        "height": 190,
+        "age": 23,
+        "gender": "male",
+        "activity_level": "moderately active",
+        "food_preferences": ["oats", "smoothies", "chicken breast"],
+        "allergies": ["nuts"],
+        "mealplan_length": 1,  # in days
+        "protein": "high",
+        "goals": "lose weight",
+        "excluded_foods": ["pork", "bacon"],
+        "eating_frequency": {"meals": 3, "snacks": 2},
+    }
+
+    # Revised Harris-Benedict Equation
+    def calculate_bmr(details):
+        if details["gender"].lower() == "male":
+            return (
+                13.397 * details["weight"]
+                + 4.799 * details["height"]
+                - 5.677 * details["age"]
+                + 88.362
+            )
+        elif details["gender"].lower() == "female":
+            return (
+                9.247 * details["weight"]
+                + 3.098 * details["height"]
+                - 4.330 * details["age"]
+                + 447.593
+            )
+        else:
+            raise ValueError("Invalid gender")
+
+    # Calculate Target Calories
+    target_calories = calculate_bmr(user_details)
+
+    # Example retrieve a meal plan
+    # Retrieve candidate recipes from Firestore based on user preferences
+
+    protein_requirements = {"normal": 7, "high": 10, "very high": 15}
 
 
+    ### WE ARE HERE 
 
-# Example retrieve a mealplan 
+    breakfast_query = (
+        collection_ref.where(
+            "nutrients.kcal", "<", 600
+        )  # Adjust based on meal plan length and distribution
+        .where(
+            "nutrients.protein", ">", protein_requirements[user_details["protein"]]
+        )  # Adjust based on high protein
+        .where("ingredients", "not-in", user_details["excluded_foods"])
+        .where("ratings", ">", 3)
+        .where("subcategory", "=", "breakfast")
+    )
 
-# # 1. Retrieve candidate recipes from Firestore
-# query = (
-#     db.collection('recipes')
-#     .where('nutrients.kcal', '<', 400)
-#     .where('nutrients.protein', '>', 10)
-#     .where('ingredients', 'array-contains', 'banana')
-# )
+    query = (
+        collection_ref.where(
+            "nutrients.kcal", "<", target_calories / 2
+        )  # Adjust based on meal plan length and distribution
+        .where(
+            "nutrients.protein", ">", protein_requirements[user_details["protein"]]
+        )  # Adjust based on high protein
+        .where("ingredients", "not-in", user_details["excluded_foods"])
+        .where("ratings", ">", 3)
+    )
+    # think about getting breakfast, lunch dinner and 2 snacks.
 
-# candidate_recipes = [doc.to_dict() for doc in query.stream()]
+    # think about food_preferences increasing the weighting of those options instead of it only being those
+    # .where('ingredients', 'array-contains-any', user_details["food_preferences"])
 
-# # 2. Accumulate Recipes until Total kcal > 2000
-# total_kcal = 0
-# filtered_recipes = []
+    # think about adding target macros
 
-# for recipe in candidate_recipes:
-#     recipe_kcal = recipe.get('nutrients', {}).get('kcal', 0)
-#     total_kcal += recipe_kcal
-#     filtered_recipes.append(recipe)
+    candidate_recipes = [doc.to_dict() for doc in query.stream()]
 
-#     if total_kcal > 2000 and len(filtered_recipes) >= 3:
-#         break
+    # Shuffle candidate recipes for variety
+    random.shuffle(candidate_recipes)
 
-# # 3. Check if Accumulated Recipes Meet the Criteria
-# if total_kcal > 2000 and len(filtered_recipes) >= 3:
-#     print(f"Found at least 3 recipes with total kcal > 2000. Total kcal: {total_kcal}")
-#     for i, recipe in enumerate(filtered_recipes):
-#         print(f"Recipe {i + 1}: {recipe['name']} - kcal: {recipe.get('nutrients', {}).get('kcal', 0)}")
-# else:
-#     print("Not enough recipes meet the conditions.")
+    # Accumulate Recipes until Total kcal > target_calories
+    total_kcal = 0
+    filtered_recipes = []
+
+    for recipe in candidate_recipes:
+        recipe_kcal = recipe.get("nutrients", {}).get("kcal", 0)
+        total_kcal += recipe_kcal
+        filtered_recipes.append(recipe)
+
+        if total_kcal > target_calories and len(filtered_recipes) >= (
+            user_details["eating_frequency"]["meals"]
+            + user_details["eating_frequency"]["snacks"]
+        ):
+            break
+
+    # Check if Accumulated Recipes Meet the Criteria
+    if total_kcal > target_calories and len(filtered_recipes) >= (
+        user_details["eating_frequency"]["meals"]
+        + user_details["eating_frequency"]["snacks"]
+    ):
+        print(
+            f"Found enough recipes for a day with total kcal > {target_calories}. Total kcal: {total_kcal}"
+        )
+        for i, recipe in enumerate(filtered_recipes):
+            print(
+                f"Recipe {i + 1}: {recipe['name']} - kcal: {recipe.get('nutrients', {}).get('kcal', 0)}"
+            )
+    else:
+        print("Not enough recipes meet the conditions.")
+
+    parsed_response = jsonify(filtered_recipes)
+
+    return parsed_response
